92080a73a9aa5aa4beb6a7973d6c7b86d68726c1
Carter Irish <2023c.irish@gmail.com>
2026-04-08 21:45:13 -0400

    Feat: Add logout and signup routes with secure connection middleware

--- server/router.ts
+++ server/router.ts
@@ -7,6 +7,11 @@ const router = (app: Application) => {
     app.get('/dashboard', middleware.secureConnect, middleware.requiresLogin, controllers.Dashboard.DashboardPage);
 
     app.get('/login', middleware.secureConnect, middleware.requiresLogout, controllers.Account.LoginPage);
+    app.post('/login', middleware.secureConnect, middleware.requiresLogout, controllers.Account.login);
+
+    app.get('/logout', middleware.secureConnect, middleware.requiresLogin, controllers.Account.logout);
+    
+    app.post('/signup', middleware.secureConnect, middleware.requiresLogout, controllers.Account.signup);
 
     app.get('/settings', middleware.secureConnect, middleware.requiresLogin,controllers.Account.settingsPage);
 
