6f0a07ffb24f53b5f8050f6ddf962d2df4a99a3c
Carter Irish <2023c.irish@gmail.com>
2026-04-08 23:09:28 -0400

    Feat: Refactor app initialization to include MongoDB and Redis connection handling

--- server/app.ts
+++ server/app.ts
@@ -8,23 +8,52 @@ import favicon from 'serve-favicon';
 import { engine } from 'express-handlebars';
 import router from './router';
 
+import mongoose from 'mongoose';
+import session from 'express-session';
+import {RedisStore} from 'connect-redis';
+import redis from 'redis';
+
 const PORT = process.env.PORT || process.env.NODE_PORT;
-const app: Express = express();
+const dbURI = process.env.MOGODB_URI || 'mongodb://localhost/Vigil';
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
-app.use(favicon(path.resolve(`${__dirname}/../hosted/img/favicon.png`)));
-app.use(compression());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+mongoose.connect(dbURI).catch((err) => {
+    if (err) {
+        console.log('Failed to connect to MongoDB');
+        throw err;
+    }
+})
 
-app.engine('handlebars', engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', path.resolve(`${__dirname}/../views`));
+const redisClient = redis.createClient({ url: process.env.REDISCLOUD_URL });
 
-router(app);
+redisClient.on('error', err => console.log('Redis Client Error', err));
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}. View at http://localhost:${PORT}`);
+redisClient.connect().then(() => {
+    const app: Express = express();
+
+    app.use(helmet());
+    app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+    app.use(favicon(path.resolve(`${__dirname}/../hosted/img/favicon.png`)));
+    app.use(compression());
+    app.use(express.urlencoded({ extended: true }));
+    app.use(express.json());
+
+    app.use(session({
+        name: 'sessionid',
+        store: new RedisStore({
+            client:redisClient
+        }),
+        secret: process.env.SESSION_SECRET!,
+        resave: false,
+        saveUninitialized: false
+    }));
+
+    app.engine('handlebars', engine({ defaultLayout: '' }));
+    app.set('view engine', 'handlebars');
+    app.set('views', path.resolve(`${__dirname}/../views`));
+
+    router(app);
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}. View at http://localhost:${PORT}`);
+    });
 });
-
