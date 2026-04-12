55f633cd7168b0fcfd744f2517a80a85669f70f6
Carter Irish <2023c.irish@gmail.com>
2026-04-11 23:12:12 -0400

    feat: Created widgets via API are sent to database

--- server/controllers/Widget.ts
+++ server/controllers/Widget.ts
@@ -1,9 +1,39 @@
 import { Request, Response } from 'express';
-
+import ServerHealthWidgetModel from '../models/ServerHealthWidget';
 //* Controllers for generic widget operations
 
 export const createWidget = async (req: Request, res: Response) => {
     // TODO: Implementation for creating a new widget
+    if(!req.body.type || !req.body.name) {
+        return res.status(400).json({error: 'Widget type and name are required'});
+    }
+
+    if(!req.session || !req.session.account) {
+        return res.status(401).json({error: 'Unauthorized'});
+    }
+
+    const baseData = {
+        name: req.body.name,
+        type: req.body.type,
+        owner: req.session.account._id
+    };
+
+    switch(baseData.type) {
+        case 'ServerHealth':{
+            if(!req.body.endpoint){
+                return res.status(400).json({error: 'Endpoint is required for ServerHealth widget'});
+            }
+            try{
+            const widgetData = {...baseData, endpoint: req.body.endpoint};
+            const newWidget = new ServerHealthWidgetModel(widgetData);
+            await newWidget.save();
+            return res.status(201).json({name: newWidget.name, type: newWidget.type, endpoint: newWidget.endpoint});}
+            catch(err: Error | unknown){
+                return res.status(500).json({error: 'Failed to create widget'});
+            }
+        }
+        default: return res.status(400).json({error: 'Invalid widget type'});
+    };
 };
 
 export const deleteWidget = async (req: Request, res: Response) => {
@@ -24,7 +54,6 @@ export const HealthWidget = async (req: Request, res: Response) => {
         const healthData = await healthResponse.json();
         res.status(200).json(healthData);
     } catch (err: Error | unknown) {
-        console.error('Error fetching health data:', err instanceof Error ? err.message : err);
         return res.status(500).json({ error: 'Failed to fetch health data' });
     }
 
